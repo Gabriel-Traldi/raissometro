9ca2f3d87cbe5431dd3b2600a90e2bea11164de1
Gabriel Traldi <traldi13@hotmail.com>
2024-12-24 17:42:12 -0300

    feat: icon and download option

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import html2canvas from "html2canvas";
+
+import bg from "../public/bg.png";
 
 const phrases = [
   "Esse é meu último natal aqui em casa.",
@@ -26,6 +29,7 @@ const phrases = [
   "Cadê minha Heineken?",
   "Você não me ama mais?",
   "Não tô em casa ~ela em casa",
+  "Tênis novo???? ~o tênis branco, que está preto depois de você usar por 2 anos consecutivos"
 ]
 
 function getRandomIndexByArrayLength(except?: number) {
@@ -42,6 +46,8 @@ function getRandomIndexByArrayLength(except?: number) {
 function App() {
   const [currentPhrase, setCurrentPhrase] = useState<number>(getRandomIndexByArrayLength());
 
+  const ref = useRef<HTMLDivElement>(null);
+
   function drawWord() {
     setCurrentPhrase((currentIndex) => getRandomIndexByArrayLength(currentIndex));
   }
@@ -52,6 +58,26 @@ function App() {
     }
   }
 
+  async function downloadImage() {
+    if (!ref.current) {
+      return;
+    }
+
+    const canvas = await html2canvas(ref.current, { windowWidth: 800, onclone(_, element) {
+      element.classList.add("teste");
+    }, });
+    canvas.toBlob((blob) => {
+      if (blob) {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'screenshot.png';
+        link.click();
+        link.remove();
+      }
+    })
+  }
+
   useEffect(() => {
     window.addEventListener("keyup", handlePressSpace);
     return () => {
@@ -60,16 +86,23 @@ function App() {
   }, []);
 
   return (
-    <main className="min-h-screen w-full bg-red-600/95 flex items-center py-16">
+    <main className="min-h-screen w-full bg-red-600/95 flex flex-col gap-12 items-center py-16 [&:not(.teste)>.only-screenshot]:hidden" ref={ref}>
+      <div className="fixed top-1/2 -translate-y-1/2 -translate-x-1/2 scale-90 only-screenshot">
+        <img src={bg} alt="" />
+      </div>
       <div className="m-auto flex flex-col justify-center items-center gap-8 text-center">
         <h3 className="text-2xl sm:text-5xl text-white uppercase font-light hyphens-auto py-6">Qual meu mood Raíssa de hoje?</h3>
         <h2 className="text-8xl/[6rem] sm:text-[10rem]/[8.5rem] font-black px-8 sm:px-24 whitespace-pre-line text-balance uppercase hyphens-auto word-break highlight-font">
           {phrases[currentPhrase]}
         </h2>
-        <span className="text-xl sm:text-2xl leading-6 bg-transparent border-none text-red-900 cursor-pointer mt-12" role="button" onClick={drawWord}>
+        <span className="text-xl sm:text-2xl leading-6 bg-transparent border-none text-red-900 cursor-pointer mt-12" role="button" onClick={drawWord} data-html2canvas-ignore>
           Aperte <span className="border border-red-900 rounded-md px-4 py-1 text-inherit align-middle inline-flex items-center mx-2">Space</span> ou clique
         </span>
       </div>
+      <button className="sm:fixed sm:right-8 sm:top-1/2 sm:-translate-y-1/2 rounded-full bg-transparent" onClick={downloadImage} data-html2canvas-ignore>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="7 10 12 15 17 10" /><line x1="12" x2="12" y1="15" y2="3" /></svg>
+      </button>
+      <div className="h-16 only-screenshot" />
     </main>
   )
 }
